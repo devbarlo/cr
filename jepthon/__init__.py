79566e39c8374957916ca3c4c246c65caa825436
devbarlo <100007607+devbarlo@users.noreply.github.com>
2022-09-15 16:06:27 +0200

    Update __init__.py

--- jepthon/__init__.py
+++ jepthon/__init__.py
@@ -1,25 +1,23 @@
-import time
-
+mport time
 import heroku3
-
 from .Config import Config
 from .core.logger import logging
-from .core.session import jepthon
+from .core.session import iqthon
 from .sql_helper.globals import addgvar, delgvar, gvarstatus
-
-
-LOGS = logging.getLogger("jepthon")
-bot = jepthon
-
+version = "7.3"
+license = "GNU Affero General Public License v3.0"
+author = "<t.me/cr_source>"
+copyright = "TeleOniOn AR (C) 2022 - 2023  " + author
+iqthon.version = version
+iqthon.tgbot.version = version
+LOGS = logging.getLogger("cr_source")
+bot = iqthon
 StartTime = time.time()
-JMVERSION = "5.2.0"
-
-
-if Config.UPSTREAM_REPO == "cr":
+catversion = "7.2"
+if Config.UPSTREAM_REPO == "TeleOniOn":
     UPSTREAM_REPO_URL = "https://github.com/devbarlo/cr"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
-
 if Config.PRIVATE_GROUP_BOT_API_ID == 0:
     if gvarstatus("PRIVATE_GROUP_BOT_API_ID") is None:
         Config.BOTLOG = False
@@ -34,7 +32,6 @@ else:
     else:
         Config.BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
     Config.BOTLOG = True
-
 if Config.PM_LOGGER_GROUP_ID == 0:
     if gvarstatus("PM_LOGGER_GROUP_ID") is None:
         Config.PM_LOGGER_GROUP_ID = -100
@@ -42,32 +39,25 @@ if Config.PM_LOGGER_GROUP_ID == 0:
         Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
 elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
     Config.PM_LOGGER_GROUP_ID = int("-" + str(Config.PM_LOGGER_GROUP_ID))
+
 try:
     if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
-        HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
-            Config.HEROKU_APP_NAME
-        ]
+        HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[Config.HEROKU_APP_NAME]
     else:
         HEROKU_APP = None
 except Exception:
     HEROKU_APP = None
-
-
-# Global Configiables
 COUNT_MSG = 0
+ISAFK = False
+AFKREASON = None
 USERS = {}
 COUNT_PM = {}
 LASTMSG = {}
 CMD_HELP = {}
-ISAFK = False
-AFKREASON = None
 CMD_LIST = {}
 SUDO_LIST = {}
-# for later purposes
-INT_PLUG = ""
 LOAD_PLUG = {}
-
-# Variables
+INT_PLUG = ""
 BOTLOG = Config.BOTLOG
 BOTLOG_CHATID = Config.BOTLOG_CHATID
 PM_LOGGER_GROUP_ID = Config.PM_LOGGER_GROUP_ID
